fb0b4fda7345c67e79361efe67eac40eb7507312
Oyatilloh Qobiljonov <oyatillohqobiljonov81@gmail.com>
2024-05-08 11:12:06 +0500

    done

---
+++ index.js
@@ -0,0 +1,64 @@
+// 1- vazifa
+
+// let son1 = Number(prompt("Birinchi sonni kiriting!"));
+// let son2 = Number(prompt("Ikkinchi sonni kiriting!"));
+
+// alert(son1 + son2);
+
+
+// 2- vazifa
+
+// let a = "12";
+// let b = Number(a);
+
+// alert (b)
+
+// 3 -vazifa
+
+    // let taxminiyson = Math.floor(Math.random() * 1000);
+
+
+    // if ( taxminiyson % 2 === 0 ) {
+    //     console.log( taxminiyson + " juft" )
+    // } else {
+    //     console.log( taxminiyson + " toq ")
+    // }
+
+// 4 -vazifa
+
+// let soz = prompt('Soz kiriting')
+
+// if ( soz === 'Mars') {
+//     alert('Mars')
+// } else {
+//     alert('Mars IT School')
+// }
+
+// 5 -vazifa
+
+//let sozlar = ["moshina", "ismoil", "hp", "salam", "qalesan", "telefon", "kitob", "stul", "stol"];
+// let natija = [];
+
+// for (let i = 0; i < 9; i++) {
+//     console.log(sozlar)
+//      if (sozlar[i].length > 5) {
+//     natija.push(sozlar[i])
+//      }
+// }
+//  console.log(natija)
+
+// 6 -vazifa
+
+// let sonlar = [2, 4, 9, 10, 45, 67, 8, 90, 98, 100];
+// let toq = [9, 45, 67,];
+// let juft = []
+
+// for (let i = 0; i < 10; i++) {
+//      if (sonlar[i] % 2 === 0) {
+//     juft.push(sonlar[i]);
+//      }
+//     }
+
+// console.log(sonlar);
+// console.log(toq);
+// console.log(juft);
